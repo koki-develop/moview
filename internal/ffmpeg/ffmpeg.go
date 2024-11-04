c91ea3ca72dfbe6cf5d0e9322a7d80ca2fe58f57
koki-develop <kou.pg.0131@gmail.com>
2024-11-04 09:43:41 +0900

    Implement ffmpeg.MovieToImages

---
+++ internal/ffmpeg/ffmpeg.go
@@ -0,0 +1,37 @@
+package ffmpeg
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sort"
+)
+
+func MovieToImages(input string) ([]string, error) {
+	probe, err := FFProbe(input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to probe video: %w", err)
+	}
+
+	// Create tmp directory
+	dir, err := os.MkdirTemp("", "moview")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create tmp directory: %w", err)
+	}
+
+	// Execute ffmpeg
+	cmd := exec.Command("ffmpeg", "-i", input, "-vf", fmt.Sprintf("fps=fps=%f", probe.FrameRate), filepath.Join(dir, "%d.jpg"))
+	if err := cmd.Run(); err != nil {
+		return nil, fmt.Errorf("failed to execute ffmpeg: %w", err)
+	}
+
+	// Get list of generated files
+	files, err := filepath.Glob(filepath.Join(dir, "*.jpg"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to list generated files: %w", err)
+	}
+
+	sort.Strings(files)
+	return files, nil
+}
