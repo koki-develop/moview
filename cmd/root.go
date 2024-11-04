04b9b76248c4b3ec1a304e1b9cd1c1c32a263874
koki-develop <kou.pg.0131@gmail.com>
2024-11-04 11:55:09 +0900

    Check for exsistence

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/koki-develop/moview/internal/ui"
@@ -11,7 +12,16 @@ var rootCmd = &cobra.Command{
 	Use:  "moview",
 	Args: cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if err := ui.Start(&ui.Option{Path: args[0]}); err != nil {
+		p := args[0]
+
+		if _, err := os.Stat(p); err != nil {
+			if os.IsNotExist(err) {
+				return fmt.Errorf("file does not exist: %s", p)
+			}
+			return err
+		}
+
+		if err := ui.Start(&ui.Option{Path: p}); err != nil {
 			return err
 		}
 		return nil
