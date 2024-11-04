f1b9b48526f6332de0fd279ea53ea38fd0201cb7
koki-develop <kou.pg.0131@gmail.com>
2024-11-04 11:38:51 +0900

    Implement ascii.Converter

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/koki-develop/moview
 go 1.21.0
 
 require (
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/qeesung/image2ascii v1.0.1
 	github.com/spf13/cobra v1.7.0
 )
@@ -11,7 +12,6 @@ require (
 	github.com/aybabtme/rgbterm v0.0.0-20170906152045-cc83f3b3ce59 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/wayneashleyberry/terminal-dimensions v1.1.0 // indirect
---
+++ internal/ascii/ascii.go
@@ -0,0 +1,40 @@
+package ascii
+
+import (
+	"image"
+	"image/color"
+	"strings"
+
+	"github.com/qeesung/image2ascii/ascii"
+	"github.com/qeesung/image2ascii/convert"
+)
+
+type Converter struct {
+	resizeHandler  *convert.ImageResizeHandler
+	pixelConverter ascii.PixelConverter
+}
+
+func NewConverter() *Converter {
+	return &Converter{
+		resizeHandler:  convert.NewResizeHandler().(*convert.ImageResizeHandler),
+		pixelConverter: ascii.NewPixelConverter(),
+	}
+}
+
+func (c *Converter) ImageToASCII(img image.Image) ([]string, error) {
+	sz := img.Bounds()
+	w := sz.Max.X
+	h := sz.Max.Y
+
+	rows := make([]string, 0, h)
+	for i := 0; i < h; i++ {
+		b := new(strings.Builder)
+		for j := 0; j < w; j++ {
+			pixel := color.NRGBAModel.Convert(img.At(j, i))
+			char := c.pixelConverter.ConvertPixelToASCII(pixel, &ascii.DefaultOptions)
+			b.WriteString(char)
+		}
+		rows = append(rows, b.String())
+	}
+	return rows, nil
+}
