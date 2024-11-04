055e6f28e32029439d9964226f5ac37aad376daf
koki-develop <kou.pg.0131@gmail.com>
2024-11-04 11:49:17 +0900

    Implement root command

--- cmd/root.go
+++ cmd/root.go
@@ -3,11 +3,19 @@ package cmd
 import (
 	"os"
 
+	"github.com/koki-develop/moview/internal/ui"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use: "moview",
+	Use:  "moview",
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := ui.Start(&ui.Option{Path: args[0]}); err != nil {
+			return err
+		}
+		return nil
+	},
 }
 
 func Execute() {
